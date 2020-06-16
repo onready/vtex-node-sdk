c453e12d2a2f11e96e039398d8f9f0694004c464
Juan Cernadas <juan@onready.com.ar>
2020-06-16 17:33:19 -0300

    add shipping policies api

--- src/modules/logistics/apis/shipping-policies/ShippingPolicies.ts
+++ src/modules/logistics/apis/shipping-policies/ShippingPolicies.ts
@@ -1,5 +1,7 @@
 import { AbstractApi } from '../../../AbstractApi';
 import { VtexHttpResponse } from '../../../../utils/VtexHttpResponse';
+import { CreateShippingPolicyRequest } from './requests/CreateShippingPolicyRequest';
+import { UpdateShippingPolicyRequest } from './requests/UpdateShippingPolicyRequest';
 
 export class ShippingPolicies extends AbstractApi {
   private static readonly BASE_PATH: string = '/api/logistics/pvt/shipping-policies';
@@ -8,8 +10,44 @@ export class ShippingPolicies extends AbstractApi {
    * Lists existing shipping policies from carriers in your store, searching by their IDs.
    * @param {string} id
    */
-  retrieveShippingPoliciesById(id: string): Promise<VtexHttpResponse> {
+  retrieveShippingPolicyById(id: string): Promise<VtexHttpResponse> {
     const path = `${ShippingPolicies.BASE_PATH}/${id}`;
     return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Deletes existing shipping policies from carriers in your store, searching by their IDs.
+   * @param {string} id
+   */
+  deleteShippingPolicyById(id: string): Promise<VtexHttpResponse> {
+    const path = `${ShippingPolicies.BASE_PATH}/${id}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.DELETE);
+  }
+
+  /**
+   * Lists existing shipping policies from carriers in your store.
+   */
+  listShippingPolicies(): Promise<VtexHttpResponse> {
+    return this.vtexHttpClient.performRequest(ShippingPolicies.BASE_PATH, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Creates new shipping policies from carriers in your store.
+   * @param {CreateShippingPolicyRequest} shippingPolicy
+   */
+  createShippingPolicy(shippingPolicy: CreateShippingPolicyRequest): Promise<VtexHttpResponse> {
+    return this.vtexHttpClient
+      .performRequest(ShippingPolicies.BASE_PATH, this.HTTP_METHODS.POST, shippingPolicy);
+  }
+
+  /**
+   * Updates information on existing Shipping Policies from carriers.
+   * @param {string} id
+   * @param {UpdateShippingPolicyRequest} shippingPolicy
+   */
+  updateShippingPolicy(id: string, shippingPolicy: UpdateShippingPolicyRequest)
+    : Promise<VtexHttpResponse> {
+    const path = `${ShippingPolicies.BASE_PATH}/${id}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.PUT, shippingPolicy);
   }
 }
