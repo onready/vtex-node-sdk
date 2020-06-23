172b46f47a8c2bb6adb5e4ddda4b3f3330b9faab
Juan Cernadas <juan@onready.com.ar>
2020-06-23 12:34:44 -0300

    finish category and brand api

--- src/modules/catalog/apis/category-and-brand/CategoryAndBrand.ts
+++ src/modules/catalog/apis/category-and-brand/CategoryAndBrand.ts
@@ -1,9 +1,13 @@
 import { AbstractApi } from '../../../AbstractApi';
 import { VtexHttpResponse } from '../../../../utils/VtexHttpResponse';
 import { GetCategoryTreeResponseItem } from './responses/GetCategoryTreeResponseItem';
+import { GetCategoryByIdResponse } from './responses/GetCategoryByIdResponse';
+import { UpdateCategoryRequest } from './requests/UpdateCategoryRequest';
+import { GetBrandResponse } from './responses/GetBrandResponse';
+import { UpdateBrandRequest } from './requests/UpdateBrandRequest';
 
 export class CategoryAndBrand extends AbstractApi {
-  private static readonly BASE_PATH: string = '/api/catalog_system';
+  private static readonly BASE_PATH: string = '/api/catalog';
 
   /**
    * Retrieves the Category Tree of your store.
@@ -13,7 +17,83 @@ export class CategoryAndBrand extends AbstractApi {
    */
   getCategoryTree(categoryLevels?: number)
     : Promise<VtexHttpResponse<Array<GetCategoryTreeResponseItem>>> {
-    const path = `${CategoryAndBrand.BASE_PATH}/pub/category/tree/${categoryLevels || ''}`;
+    const path = `${CategoryAndBrand.BASE_PATH}_system/pub/category/tree/${categoryLevels || ''}`;
     return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Retrieves general information about a Category
+   * @param {number} categoryId
+   */
+  getCategoryById(categoryId: number): Promise<VtexHttpResponse<GetCategoryByIdResponse>> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/category/${categoryId}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Updates a previously existing Category
+   * @param {number} categoryId
+   * @param {UpdateCategoryRequest} data
+   */
+  updateCategory(categoryId: number, data: UpdateCategoryRequest)
+    : Promise<VtexHttpResponse<GetCategoryByIdResponse>> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/category/${categoryId}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.PUT, data);
+  }
+
+  /**
+   * Creates a new Category from scratch
+   * @param {UpdateCategoryRequest} data
+   */
+  createCategory(data: UpdateCategoryRequest)
+    : Promise<VtexHttpResponse<GetCategoryByIdResponse>> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/category`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.POST, data);
+  }
+
+  /**
+   * Retrieves all Brands registered in the store's Catalog.
+   */
+  getBrandList(): Promise<VtexHttpResponse<Array<GetBrandResponse>>> {
+    const path = `${CategoryAndBrand.BASE_PATH}_system/pvt/brand/list`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Retrieves a specific Brand by its ID.
+   * @param {string} brandId
+   */
+  getBrand(brandId: string): Promise<VtexHttpResponse<GetBrandResponse>> {
+    const path = `${CategoryAndBrand.BASE_PATH}_system/pvt/brand/${brandId}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.GET);
+  }
+
+  /**
+   * Updates a previously existing Brand
+   * @param {string} brandId
+   * @param {UpdateBrandRequest} brandData
+   */
+  updateBrand(brandId: string, brandData: UpdateBrandRequest)
+    : Promise<VtexHttpResponse<UpdateBrandRequest>> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/brand/${brandId}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.PUT, brandData);
+  }
+
+  /**
+   * Deletes an existing Brand
+   * @param {string} brandId
+   */
+  deleteBrand(brandId: string): Promise<VtexHttpResponse> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/brand/${brandId}`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.DELETE);
+  }
+
+  /**
+   * Creates a new Brand from scratch
+   * @param {UpdateBrandRequest} brandData
+   */
+  createBrand(brandData: UpdateBrandRequest): Promise<VtexHttpResponse<UpdateBrandRequest>> {
+    const path = `${CategoryAndBrand.BASE_PATH}/pvt/brand`;
+    return this.vtexHttpClient.performRequest(path, this.HTTP_METHODS.POST, brandData);
   }
 }
